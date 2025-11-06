30647eb1a2e92fae9f9a8c476751d2ee07884cd0
Rodrigo Menendez Molina <menendez.rodrigo555@gmail.com>
2025-11-06 11:04:48 +0000

    2

--- SimulacroTrimestr1.2/simulacroProyecto.py
+++ SimulacroTrimestr1.2/simulacroProyecto.py
@@ -1,6 +1,5 @@
-
 import mysql.connector
-from flask import Flask
+from flask import Flask, render_template_string
 
 
 #Conexion de suusario para mysql
@@ -11,9 +10,8 @@ conexion = mysql.connector.connect(
     database="simulacro"
 )
 
-############################################################################
+########################################################################################
 
-#Cramos el cursor y ejecutamos al consulta, mostramos la vision de piezas y categorias
 cursor = conexion.cursor()
 
 cursor.execute('''SELECT *FROM vista_piezas;''')
@@ -24,27 +22,84 @@ filas = cursor.fetchall()
 #Mostramos las filas
 for fila in filas:
     titulo,descripcionP,imagen,categoria,descripcionC = fila
-    print(fila)
-    print(titulo,descripcionP,imagen,categoria,descripcionC)
 
-###############################################################################
+#########################################################################################
 
+#Creamos una palicacion flask (osea web)
 app = Flask(__name__)
 
-
+#Atrapo la ruta raiz
 @app.route("/")
+
 def raiz():
-    return "cadena"
+    print("Funciona")
+    cadena =  '''
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <title>Simulacro2</title>
+        <meta charset="UTF-8">
+
+        <style>
+                body{                                   
+                    background:rgb(220, 238, 236);     
+                    font-family:monospace;              
+                }
+                header,main,footer{
+                    width:600px;         
+                    background:ghostwhite;
+                    margin:auto;                
+                    padding:20px;
+                    text-align: center;     /*Para que el textoe ste centrado*/
+                }
+                header{
+                    background: rgb(226, 226, 233);
+                }
+                footer{
+                    background: rgb(226, 226, 233);
+                }
+                main{
+                    display: grid;
+                    grid-template-columns: auto auto auto; /*El numero de auto son el numero de columnas*/
+                    gap: 20px;
+                }
+                main img{
+                    width: 100%;
+                    height: auto;
+                    margin: 0px;
+                }
+
+
+        </style>
+    </head>
+
+
+    <body>
+        <header>
+            <h1>Rodrigo Menendez Molina</h1>
+            <h2>menendez.rodrigo555@gmail.com</h2>
+        </header>
+        <main>
+    '''
+    for fila in filas:
+        titulo,descripcionP,imagen,categoria,descripcionC = fila
+        cadena +=   ("<article>"
+                        "<h3>"+str(titulo)+"</h3>"
+                        "<img src="+str(imagen)+" alt='En proceso'></img>"
+                        "<p>"+str(categoria)+"</p>"
+                        "<p>"+str(descripcionP)+"</p>"
+                    "</article>")
+    cadena +='''
+        </main>
+        <footer>
+            <p>© 2025 Rodrigo Menendez Molina. Todos los derechos reservados.</p>
+        </footer>
+    </body>
+
+
+    </html>
+    '''
+    return cadena
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-'''
-titulo
-descripcion pieza
-imagen
-categoria
-descripcion categoria
-'''
