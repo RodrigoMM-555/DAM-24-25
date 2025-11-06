18ce035456d9adf898d7e8c75a2d6e73b76b04fd
Rodrigo Menendez Molina <menendez.rodrigo555@gmail.com>
2025-11-06 13:26:05 +0000

    7

---
+++ Entornos de desarrollo/001 Desarrollo de software/Tipos de codigo/Tipos de codigo.py
@@ -0,0 +1,9 @@
+print("Hola mundo")
+
+#En consola mentemos python3 -m py_compile [nombre del .py]
+
+#Esto crea una carpeta llamda pycache en el directorio del .py
+
+#codigo fuente, el que usamos
+#codigo objeto, es rapido y multiplataforma (creo que es el que tiene aplicaciones .exe)
+#codigo ejecutable, el que entiende la maquina
