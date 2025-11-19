6270ad3f0f5bb4d967f31aac61dddc10df6dcfbc
Rodrigo Menendez Molina <menendez.rodrigo555@gmail.com>
2025-11-19 13:08:55 +0000

    4

---
+++ Bses de datos/U3 Relacion de consultas/004 Agrupamiento de registros/006Barra.py
@@ -0,0 +1,35 @@
+
+import mysql.connector
+#import matplotlib      /Esto lo he usado porque no funcionaba antes
+#matplotlib.use('TkAgg')  # Use TkAgg for local environments
+import matplotlib.pyplot as pt
+
+
+conexion = mysql.connector.connect(
+    host = "localhost",
+    user = "Uclientes",
+    password = "Contra1$",
+    database = "clientes"
+)
+
+cursor = conexion.cursor()
+cursor.execute('''
+    SELECT COUNT(color), color
+    FROM productos
+    GROUP BY color
+    ORDER BY COUNT(color) ASC;
+''')
+
+filas = cursor.fetchall()
+
+cantidades = []
+etiquetas = []
+for fila in filas:
+    cantidades.append(fila[0])
+    etiquetas.append(fila[1])
+print(cantidades)
+print(etiquetas)
+
+colores = ['red','green','blue']
+pt.bar(cantidades,height=30)
+pt.show()
