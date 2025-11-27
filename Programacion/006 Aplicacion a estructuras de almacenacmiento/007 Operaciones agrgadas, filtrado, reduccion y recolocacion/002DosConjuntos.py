c5b535ce1edb97ff1578d0568745a6af7bcefc48
Rodrigo Menendez Molina <menendez.rodrigo555@gmail.com>
2025-11-27 11:42:57 +0000

    a

---
+++ Programacion/006 Aplicacion a estructuras de almacenacmiento/007 Operaciones agrgadas, filtrado, reduccion y recolocacion/002DosConjuntos.py
@@ -0,0 +1,15 @@
+
+# No se aplica el concepto de orden
+# No se pueden reordenar
+# No admiten duplicados
+
+frutas1 = {"manzanas","peras","platanos"}
+
+frutas2 = {"peras","platanos","manzanas"}
+
+if frutas1 == frutas2:
+    print("Son iguales")
+else:
+    print("Son diferentes")
+
+#El orden no importa
---
+++ Programacion/006 Aplicacion a estructuras de almacenacmiento/007 Operaciones agrgadas, filtrado, reduccion y recolocacion/010SudokuMal.py
@@ -0,0 +1,20 @@
+import random
+
+patron = {1,2,3,4,5,6,7,8,9}
+sudoku = []
+
+#Cuando no se usa un avariable como "celda" se recomienda usar un _
+for celda in range(1,10):
+    while True:
+        lista = []
+
+        for i in range(1,10):
+            lista.append(random.randint(1,9))
+
+        conjunto = set(lista)
+
+        if conjunto == patron:
+            sudoku.append(lista)
+            break
+
+print(sudoku)
