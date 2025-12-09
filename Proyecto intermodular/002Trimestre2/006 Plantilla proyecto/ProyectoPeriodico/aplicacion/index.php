2d7159452a72159849a0e4bfcfb8fe42f3ac6621
Rodrigo Menendez Molina <menendez.rodrigo555@gmail.com>
2025-12-09 12:34:50 +0000

    a

---
+++ Proyecto intermodular/002Trimestre2/006 Plantilla proyecto/ProyectoPeriodico/aplicacion/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="es">
+        <head>
+        <title>La tostada - Una buena rebanada de noticias</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+    <body>
+        <header>
+            <h1>La tostada</h1>
+            <h2>Noticias para desayunar</h2>
+        </header>
+        <main>
+            <?php include "inc/listar_articulos.php"; ?>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
---
+++ Proyecto intermodular/Proyecto/001 Busqueda de informaicón/000 Idea inicial/003 Ambito.md
@@ -0,0 +1,8 @@
+
+1. Escoger ambito de programacion
+
+2. Que problemas existen en ese ambito
+
+3. Que se puede hacer para resolver ese problema
+
+4. Plantear una propuesta ciega
